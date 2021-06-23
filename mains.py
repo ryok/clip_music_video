2c7451dc1905778a0ab4afb030e79b3cc42c22ef
Ryo Okada <atribecalledquest448@gmail.com>
2021-06-23 11:17:10 +0900

    :recycle: refactor

--- mains.py
+++ mains.py
@@ -69,42 +69,6 @@ sideX       = 512
 sideY       = 512
 
 
-# class ReplaceGrad(torch.autograd.Function):
-#     @staticmethod
-#     def forward(ctx, x_forward, x_backward):
-#         ctx.shape = x_backward.shape
-#         return x_forward
-
-#     @staticmethod
-#     def backward(ctx, grad_in):
-#         return None, grad_in.sum_to_size(ctx.shape)
-
-
-# replace_grad = ReplaceGrad.apply
-
-# class ClampWithGrad(torch.autograd.Function):
-#     @staticmethod
-#     def forward(ctx, input, min, max):
-#         ctx.min = min
-#         ctx.max = max
-#         ctx.save_for_backward(input)
-#         return input.clamp(min, max)
-
-#     @staticmethod
-#     def backward(ctx, grad_in):
-#         input, = ctx.saved_tensors
-#         return grad_in * (grad_in * (input - input.clamp(ctx.min, ctx.max)) >= 0), None, None
-
-
-# clamp_with_grad = ClampWithGrad.apply
-
-# def vector_quantize(x, codebook):
-#     d = x.pow(2).sum(dim=-1, keepdim=True) + codebook.pow(2).sum(dim=1) - 2 * x @ codebook.T
-#     indices = d.argmin(-1)
-#     x_q = F.one_hot(indices, codebook.shape[0]).to(d.dtype) @ codebook
-#     return replace_grad(x_q, x)
-
-
 def load_vqgan_model(config_path, checkpoint_path):
     config = OmegaConf.load(config_path)
     if config.model.target == 'taming.models.vqgan.VQModel':
